2a06556fbd2f5f74d915c3f469cda9f416854176
lee951109 <sk985326@naver.com>
2022-06-16 21:47:43 +0900

    reacr-redux 없이 연결

---
+++ src/hooks/useReduxDispatch.js
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import ReduxContext from "../contexts/ReduxContext";
+
+
+export default function useReduxDispatch() {
+  const store = useContext(ReduxContext);
+
+  return store.dispatch;
+}
--- src/index.js
+++ src/index.js
@@ -5,22 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import store from './redux/store';
-import { addTodo, completeTodo, showComplete } from './redux/actions';
+import ReduxContext from './contexts/ReduxContext';
+// import { addTodo, completeTodo, showComplete } from './redux/actions';
 
-//dispatch 가 발생하는 것 을 감시.
-// return이 unsubscribe.
-store.subscribe(() => { 
-  console.log(store.getState());
-});
+// //dispatch 가 발생하는 것 을 감시.
+// // return이 unsubscribe.
+// store.subscribe(() => { 
+//   console.log(store.getState());
+// });
 
-store.dispatch(addTodo("할일"));
-store.dispatch(completeTodo(0));
-store.dispatch(showComplete());
+// store.dispatch(addTodo("할일"));
+// store.dispatch(completeTodo(0));
+// store.dispatch(showComplete());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ReduxContext.Provider value={store}>
+     <App />
+    </ReduxContext.Provider>
   </React.StrictMode>
 );
 
